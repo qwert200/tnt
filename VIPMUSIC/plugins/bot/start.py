672e82648a3df842069cab2c068112bf310740bb
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-10 19:02:45 +0300

    Update start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -19,10 +19,9 @@ from VIPMUSIC.utils.database import (
 )
 from VIPMUSIC.utils.decorators.language import LanguageStart
 from VIPMUSIC.utils.formatters import get_readable_time
+from VIPMUSIC.utils.inline import first_page, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
-from VIPMUSIC.utils.inline import (help_pannel, private_panel,
-                                     start_pannel)
 
 
 
