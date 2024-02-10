d3be6ddb76ace956e58e0cf25456e5ec482c947e
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-10 03:18:50 +0300

    Update stop.py

--- VIPMUSIC/plugins/admins/stop.py
+++ VIPMUSIC/plugins/admins/stop.py
@@ -10,7 +10,22 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
+    filters.command(["end", "stop", "ايقاف", "انهاء"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return
+    await VIP.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
+
+
+
+@app.on_message(
+    filters.command(["end", "stop", "ايقاف", "انهاء"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.channel & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
