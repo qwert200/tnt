4d6b8ee8c449bea974e2f8712000d7064d115729
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-02-10 03:13:20 +0300

    Update resume.py

--- VIPMUSIC/plugins/admins/resume.py
+++ VIPMUSIC/plugins/admins/resume.py
@@ -9,7 +9,12 @@ from VIPMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
+@app.on_message(
+   filters.command(["resume", "استئناف"," كمل"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+            
+    & filters.group
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
@@ -20,15 +25,49 @@ async def resume_com(cli, message: Message, _, chat_id):
         [
             
             InlineKeyboardButton(
-                text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"
+                text="تخطي", callback_data=f"ADMIN Skip|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"
+                text="ايقاف", callback_data=f"ADMIN Stop|{chat_id}"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="ᴘᴀᴜsᴇ",
+                text="وقف",
+                callback_data=f"ADMIN Pause|{chat_id}",
+            ),
+        ]
+    ]
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(buttons_resume)
+    )
+
+
+@app.on_message(
+   filters.command(["resume", "استئناف"," كمل"] ,prefixes=["/", "!", "%", ",", "", ".", "@", "#"])
+            
+    & filters.channel
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    await VIP.resume_stream(chat_id)
+    buttons_resume = [
+        [
+            
+            InlineKeyboardButton(
+                text="تخطي", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="ايقاف", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="وقف",
                 callback_data=f"ADMIN Pause|{chat_id}",
             ),
         ]
